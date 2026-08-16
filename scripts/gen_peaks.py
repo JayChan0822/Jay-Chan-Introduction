8f7863a3d52abd30ccff91b05f47a94e082454fb
JayChan0822 <cy4ztyytvj@privaterelay.appleid.com>
2026-08-17 01:43:16 +0800

    remove: SOUND 区删除《鹊桥仙》曲目（含波形数据）

--- scripts/gen_peaks.py
+++ scripts/gen_peaks.py
@@ -14,7 +14,7 @@ CACHE.mkdir(exist_ok=True)
 
 STEMS = [
     "an_ye_zhu_guang", "da_jiang_hu_ming_sheng", "heng_jue",
-    "que_qiao_xian", "hang_zhou_zhi_jiang", "su_zhou_nian",
+    "hang_zhou_zhi_jiang", "su_zhou_nian",
     "shu_zhong_tian_fu", "qin_yun_bin_fen_zou_xin_chun",
     "tian_wen", "da_zhu_jiao", "chong_shi_xing_yuan", "shui_xiang_ji_yi",
     "shi_xu", "huang_yang_bian_dan",
